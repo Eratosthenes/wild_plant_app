3f74e241c740cc47e5915a00658f6597a8a43a6d
Nathan Rapport <n.rapport@gmail.com>
2016-01-17 07:39:25 -0800

    refactored search_plants method

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,20 +2,18 @@ class Search < ActiveRecord::Base
 
   def search_plants
     plants = Plant.all
-
-    plants = plants.where(["name LIKE ?","%#{name}%"]) if name.present?
-    plants = plants.where(["tsn LIKE ?", tsn]) if tsn.present?
-
-    plants = plants.where(["leaf_type LIKE ?","%#{leaf_type}%"]) if leaf_type.present?
-    plants = plants.where(["leaf_attachment LIKE ?","%#{leaf_attachment}%"]) if leaf_attachment.present?
-    plants = plants.where(["leaf_arrangement LIKE ?","%#{leaf_arrangement}%"]) if leaf_arrangement.present?
-    plants = plants.where(["leaf_veins LIKE ?","%#{leaf_veins}%"]) if leaf_veins.present?
-    plants = plants.where(["leaf_edge LIKE ?","%#{leaf_edge}%"]) if leaf_edge.present?
-
-    plants = plants.where(["bark_attachment LIKE ?","%#{bark_attachment}%"]) if bark_attachment.present?
-    plants = plants.where(["bark_texture LIKE ?","%#{bark_texture}%"]) if bark_texture.present?
-    plants = plants.where(["bark_color LIKE ?","%#{bark_color}%"]) if bark_color.present?
-
+    #plants = plants.where(["name LIKE ?","%#{name}%"]) if name.present?
+    column_names.each do |var|
+      plants = plants.where(["#{var} LIKE ?","%#{eval(var)}%"]) if eval(var).present?
+    end
     plants
+  end
+
+  def column_names
+    [:name, :leaf_type, :leaf_attachment, :leaf_arrangement, :leaf_veins, :leaf_edge,
+     :flower_type, :flower_attachment, :flower_arrangement, :flower_symmetry, :flower_petal_number,
+     :flower_petal_color, :fruit_type, :fruit_attachment, :fruit_arrangement, :fruit_color,
+     :bark_attachment, :bark_texture, :bark_color, :thorn_type, :thorn_arrangement, :thorn_stiffness,
+     :tendril_type, :tendril_attachment, :tendril_arrangement].map(&:to_s)
   end
 end
