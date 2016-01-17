738027e4646163ddd4b35b3d2543c49c48bc7c76
Nathan Rapport <n.rapport@gmail.com>
2016-01-17 00:30:22 -0800

    fixed empty search issue

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -16,6 +16,32 @@ class SearchesController < ApplicationController
 
   private
   def search_params
-      params.require(:search).permit(:name, :leaf_type, :leaf_attachment, :leaf_arrangement, :leaf_veins, :leaf_edge, :flower_type, :flower_attachment, :flower_arrangement, :flower_symmetry, :flower_petal_number, :flower_petal_color, :fruit_type, :fruit_attachment, :fruit_arrangement, :fruit_color, :bark_attachment, :bark_texture, :bark_color, :thorn_type, :thorn_arrangement, :thorn_stiffness, :tendril_type, :tendril_attachment, :tendril_arrangement, :tsn)
+      params[:search] ||= {leaf_type: 'empty search' } #prevents error on empty search
+      params.require(:search).permit(:name,
+      :leaf_type,
+      :leaf_attachment,
+      :leaf_arrangement,
+      :leaf_veins,
+      :leaf_edge,
+      :flower_type,
+      :flower_attachment,
+      :flower_arrangement,
+      :flower_symmetry,
+      :flower_petal_number,
+      :flower_petal_color,
+      :fruit_type,
+      :fruit_attachment,
+      :fruit_arrangement,
+      :fruit_color,
+      :bark_attachment,
+      :bark_texture,
+      :bark_color,
+      :thorn_type,
+      :thorn_arrangement,
+      :thorn_stiffness,
+      :tendril_type,
+      :tendril_attachment,
+      :tendril_arrangement,
+      :tsn)
   end
 end
