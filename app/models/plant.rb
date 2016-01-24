01080192073d4381fdb92d117e0200b1e3f82cbd
Nathan Rapport <n.rapport@gmail.com>
2016-01-24 06:40:24 -0800

    fixed get_details

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,6 @@
 class Plant < ActiveRecord::Base
 
   def get_details
-    PlantDetail.joins('LEFT OUTER JOIN plants ON plant_details.tsn = plants.tsn')
+    PlantDetail.joins('LEFT OUTER JOIN plants ON plant_details.tsn = plants.tsn').where('plants.tsn = ?', self.tsn)
   end
 end
