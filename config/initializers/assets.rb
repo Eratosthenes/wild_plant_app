27a507ef6837a7863f3542928b40603106246f91
Nathan Rapport <n.rapport@gmail.com>
2016-01-24 19:36:13 -0800

    added pictures for first screen

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,11 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( multistep.js )
 Rails.application.config.assets.precompile += %w( light_greenery.jpg )
+Rails.application.config.assets.precompile += %w( leaf_simple.jpg )
+Rails.application.config.assets.precompile += %w( leaf_compound.jpg )
+Rails.application.config.assets.precompile += %w( leaf_needle.jpg )
+Rails.application.config.assets.precompile += %w( leaf_scale.jpg )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
